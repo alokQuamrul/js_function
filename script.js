2608982c633505fcde4f43b6647848e9ffe57485
alokQuamrul <islam.kamrul3105@gmail.com>
2024-11-09 19:40:09 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// Declaring/Defining a Function 
+function sumOfTwoNumbers(num1,num2){
+    let sum = num1 + num2;
+    return sum;
+};
+// Calling the function and printing it 
+
+total = sumOfTwoNumbers(25,33);
+// document.getElementById("func1").innerHTML = "The total Sum = "+ sum;
+console.log("The total Sum =", sum);
